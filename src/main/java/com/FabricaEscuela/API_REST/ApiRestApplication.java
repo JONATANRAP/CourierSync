47a6aa994c4fbd8882cf575bdc2232ccd5b74322
Agudelo18 <cristian.09agudelo@gmail.com>
2025-05-21 21:27:06 -0500

    configuracion de las CORS

--- src/main/java/com/FabricaEscuela/API_REST/ApiRestApplication.java
+++ src/main/java/com/FabricaEscuela/API_REST/ApiRestApplication.java
@@ -3,6 +3,9 @@ package com.FabricaEscuela.API_REST;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
@@ -12,4 +15,15 @@ public class ApiRestApplication {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
 
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://couriersync.vercel.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
--- src/main/java/com/FabricaEscuela/API_REST/security/SecurityConfig.java
+++ src/main/java/com/FabricaEscuela/API_REST/security/SecurityConfig.java
@@ -10,11 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -26,7 +21,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // habilita CORS
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -35,20 +29,6 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-    }
-
-    // Configuración de CORS
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://couriersync.vercel.app")); // URL del frontend
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true); // importante si usas cookies o headers de autenticación
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
     }
 
     @Bean
